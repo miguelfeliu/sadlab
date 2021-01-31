3e997ecd9aac28d3edd7dd1fe8626bb248befe2d
Ferran <ferran@domatix.com>
2021-01-31 17:38:07 +0100

    cambiado test1

--- test/test1.sh
+++ test/test1.sh
@@ -1,35 +1,37 @@
+#!/bin/sh
+
 echo Iniciando test
 
 echo Iniciando frontend...
-start node ../frontend/index.js
+gnome-terminal -e "node ../frontend/index.js"
 sleep 0.5
 
 echo Iniciando broker...
-start node ../lbbroker/broker.js
+gnome-terminal -e "node ../lbbroker/broker.js"
 sleep 0.5
 
 echo Iniciando cola A...
-start node ../queue/queue.js queueA 8123
+gnome-terminal -e "node ../queue/queue.js queueA 8123"
 sleep 0.5
 
 echo Iniciando cola B...
-start node ../queue/queue.js queueB 8124
+gnome-terminal -e "node ../queue/queue.js queueB 8124"
 sleep 0.5
 
 echo Iniciando cola C...
-start node ../queue/queue.js queueC 8125
+gnome-terminal -e "node ../queue/queue.js queueC 8125"
 sleep 0.5
 
 echo Iniciando un worker...
-start node ../worker/worker.js
+gnome-terminal -e "node ../worker/worker.js"
 sleep 0.5
 
 echo Iniciando un worker...
-start node ../worker/worker.js
+gnome-terminal -e "node ../worker/worker.js"
 sleep 0.5
 
 echo Iniciando un worker...
-start node ../worker/worker.js
+gnome-terminal -e "node ../worker/worker.js"
 sleep 0.5
 
 curl --header "Content-Type: application/json" \
